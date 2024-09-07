2eec4e1cc56bfc7066c0617147fcd6f76b6afd6b
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-07 17:27:20 +0200

    integrated systemge changes

--- appChess/app.go
+++ appChess/app.go
@@ -1,19 +1,123 @@
 package appChess
 
 import (
+	"SystemgeSampleChessServer/dto"
+	"SystemgeSampleChessServer/topics"
+	"encoding/json"
 	"sync"
 
-	"github.com/neutralusername/Systemge/Node"
+	"github.com/neutralusername/Systemge/Config"
+	"github.com/neutralusername/Systemge/Error"
+	"github.com/neutralusername/Systemge/Helpers"
+	"github.com/neutralusername/Systemge/Message"
+	"github.com/neutralusername/Systemge/SingleRequestServer"
+	"github.com/neutralusername/Systemge/SystemgeConnection"
 )
 
 type App struct {
 	games map[string]*ChessGame
 	mutex sync.Mutex
+
+	singleRequestServer *SingleRequestServer.Server
 }
 
-func New() Node.Application {
+func New() *App {
 	app := &App{
 		games: make(map[string]*ChessGame),
 	}
+
+	app.singleRequestServer = SingleRequestServer.NewSingleRequestServer("chessSpawner",
+		&Config.SingleRequestServer{
+			SystemgeServerConfig: &Config.SystemgeServer{
+				ListenerConfig: &Config.TcpSystemgeListener{
+					TcpServerConfig: &Config.TcpServer{
+						TlsCertPath: "MyCertificate.crt",
+						TlsKeyPath:  "MyKey.key",
+						Port:        60001,
+					},
+				},
+				ConnectionConfig: &Config.TcpSystemgeConnection{},
+			},
+			DashboardClientConfig: &Config.DashboardClient{
+				ConnectionConfig: &Config.TcpSystemgeConnection{},
+				ClientConfig: &Config.TcpClient{
+					Address: "localhost:60000",
+					TlsCert: Helpers.GetFileContent("MyCertificate.crt"),
+					Domain:  "example.com",
+				},
+			},
+		},
+		nil, SystemgeConnection.NewConcurrentMessageHandler(
+			SystemgeConnection.AsyncMessageHandlers{},
+			SystemgeConnection.SyncMessageHandlers{
+				topics.MOVE: func(systemgeConnection SystemgeConnection.SystemgeConnection, message *Message.Message) (string, error) {
+					app.mutex.Lock()
+					defer app.mutex.Unlock()
+					move, err := dto.UnmarshalMove(message.GetPayload())
+					if err != nil {
+						return "", Error.New("Error unmarshalling move", err)
+					}
+					game := app.games[move.PlayerId]
+					if game == nil {
+						return "", Error.New("Game does not exist", nil)
+					}
+					move.GameId = game.whiteId + "-" + game.blackId
+					chessMove, err := game.handleMoveRequest(move)
+					if err != nil {
+						return "", err
+					}
+					return Helpers.JsonMarshal(chessMove), nil
+				},
+				topics.STARTGAME: func(systemgeConnection SystemgeConnection.SystemgeConnection, message *Message.Message) (string, error) {
+					ids := []string{}
+					json.Unmarshal([]byte(message.GetPayload()), &ids)
+					whiteId := ids[0]
+					blackId := ids[1]
+					game := newChessGame(whiteId, blackId)
+					app.mutex.Lock()
+					defer app.mutex.Unlock()
+					if app.games[whiteId] != nil || app.games[blackId] != nil {
+						app.mutex.Unlock()
+						return "", Error.New("Already in a game", nil)
+					}
+					app.games[whiteId] = game
+					app.games[blackId] = game
+					return game.marshalBoard(), nil
+				},
+				topics.ENDGAME: func(systemgeConnection SystemgeConnection.SystemgeConnection, message *Message.Message) (string, error) {
+					id := message.GetPayload()
+					app.mutex.Lock()
+					defer app.mutex.Unlock()
+					if app.games[id] == nil {
+						return "", Error.New("Game does not exist", nil)
+					}
+					game := app.games[id]
+					delete(app.games, game.whiteId)
+					delete(app.games, game.blackId)
+					return game.whiteId + "-" + game.blackId, nil
+				},
+			},
+			nil, nil,
+		),
+	)
+	if err := app.singleRequestServer.Start(); err != nil {
+		// shouldn't happen in this sample. Should be properly error handled in a real application though
+		panic(Error.New("Failed to start singleRequestServer", err))
+	}
+
 	return app
+}
+
+func (game *ChessGame) handleMoveRequest(move *dto.Move) (*dto.Move, error) {
+	if game.isWhiteTurn() && move.PlayerId != game.whiteId {
+		return nil, Error.New("Not your turn", nil)
+	}
+	if !game.isWhiteTurn() && move.PlayerId != game.blackId {
+		return nil, Error.New("Not your turn", nil)
+	}
+	chessMove, err := game.move(move)
+	if err != nil {
+		return nil, Error.New("Invalid move", err)
+	}
+	return chessMove, nil
 }
--- appWebsocketHTTP/app.go
+++ appWebsocketHTTP/app.go
@@ -1,16 +1,215 @@
 package appWebsocketHTTP
 
 import (
-	"github.com/neutralusername/Systemge/Node"
+	"SystemgeSampleChessServer/dto"
+	"SystemgeSampleChessServer/topics"
+	"errors"
+	"strings"
+	"sync"
+
+	"github.com/neutralusername/Systemge/Config"
+	"github.com/neutralusername/Systemge/Dashboard"
+	"github.com/neutralusername/Systemge/Error"
+	"github.com/neutralusername/Systemge/HTTPServer"
+	"github.com/neutralusername/Systemge/Helpers"
+	"github.com/neutralusername/Systemge/Message"
+	"github.com/neutralusername/Systemge/SingleRequestServer"
+	"github.com/neutralusername/Systemge/Status"
+	"github.com/neutralusername/Systemge/WebsocketServer"
 )
 
 type AppWebsocketHTTP struct {
+	status      int
+	statusMutex sync.Mutex
+
+	websocketServer *WebsocketServer.WebsocketServer
+	httpServer      *HTTPServer.HTTPServer
 }
 
 func New() *AppWebsocketHTTP {
-	return &AppWebsocketHTTP{}
+	app := &AppWebsocketHTTP{
+		status: Status.STOPPED,
+	}
+
+	app.websocketServer = WebsocketServer.New("appWebsocketHttp_websocketServer",
+		&Config.WebsocketServer{
+			ClientWatchdogTimeoutMs: 1000 * 60,
+			Pattern:                 "/ws",
+			TcpServerConfig: &Config.TcpServer{
+				Port: 8443,
+			},
+		},
+		WebsocketServer.MessageHandlers{
+			topics.STARTGAME: func(websocketClient *WebsocketServer.WebsocketClient, message *Message.Message) error {
+				whiteId := websocketClient.GetId()
+				blackId := message.GetPayload()
+				if !app.websocketServer.ClientExists(blackId) {
+					return Error.New("Opponent does not exist", nil)
+				}
+				response, err := SingleRequestServer.SyncRequest("websocketHttpServer", &Config.SingleRequestClient{
+					TcpConnectionConfig: &Config.TcpSystemgeConnection{},
+					TcpClientConfig: &Config.TcpClient{
+						Address: "localhost:60001",
+						TlsCert: Helpers.GetFileContent("MyCertificate.crt"),
+						Domain:  "example.com",
+					},
+				}, topics.STARTGAME, Helpers.JsonMarshal([]string{whiteId, blackId}))
+				if err != nil {
+					panic(Error.New("Error sending startGame message", err))
+				}
+				if response.GetTopic() != Message.TOPIC_SUCCESS {
+					panic(Error.New("Error starting game", errors.New(response.GetPayload())))
+				}
+				err = app.websocketServer.AddClientToGroup(whiteId+"-"+blackId, whiteId, blackId)
+				if err != nil {
+					panic(Error.New("Error adding clients to group", err))
+				}
+				err = app.websocketServer.Groupcast(whiteId+"-"+blackId, Message.NewAsync(topics.STARTGAME, response.GetPayload()))
+				if err != nil {
+					panic(Error.New("Error groupcasting start message", err))
+				}
+				return nil
+			},
+			topics.ENDGAME: func(websocketClient *WebsocketServer.WebsocketClient, message *Message.Message) error {
+				response, err := SingleRequestServer.SyncRequest("websocketHttpServer", &Config.SingleRequestClient{
+					TcpConnectionConfig: &Config.TcpSystemgeConnection{},
+					TcpClientConfig: &Config.TcpClient{
+						Address: "localhost:60001",
+						TlsCert: Helpers.GetFileContent("MyCertificate.crt"),
+						Domain:  "example.com",
+					},
+				}, topics.ENDGAME, websocketClient.GetId())
+				if err != nil {
+					return Error.New("Error sending endGame message", err)
+				}
+				if response.GetTopic() != Message.TOPIC_SUCCESS {
+					return Error.New("Error ending game", nil)
+				}
+				ids := strings.Split(response.GetPayload(), "-")
+				app.websocketServer.Groupcast(response.GetPayload(), Message.NewAsync(topics.ENDGAME, ""))
+				app.websocketServer.RemoveClientFromGroup(response.GetPayload(), ids...)
+				return nil
+			},
+			topics.MOVE: func(websocketClient *WebsocketServer.WebsocketClient, message *Message.Message) error {
+				move, err := dto.UnmarshalMove(message.GetPayload())
+				if err != nil {
+					return Error.New("Error unmarshalling move", err)
+				}
+				move.PlayerId = websocketClient.GetId()
+				response, err := SingleRequestServer.SyncRequest("websocketHttpServer", &Config.SingleRequestClient{
+					TcpConnectionConfig: &Config.TcpSystemgeConnection{},
+					TcpClientConfig: &Config.TcpClient{
+						Address: "localhost:60001",
+						TlsCert: Helpers.GetFileContent("MyCertificate.crt"),
+						Domain:  "example.com",
+					},
+				}, topics.MOVE, Helpers.JsonMarshal(move))
+				if err != nil {
+					return Error.New("Error sending move message", err)
+				}
+				if response.GetTopic() != Message.TOPIC_SUCCESS {
+					return Error.New("Error making move", nil)
+				}
+				responseMove, err := dto.UnmarshalMove(response.GetPayload())
+				if err != nil {
+					return Error.New("Error unmarshalling response move", err)
+				}
+				app.websocketServer.Groupcast(responseMove.GameId, Message.NewAsync(topics.MOVE, Helpers.JsonMarshal(responseMove)))
+				return nil
+			},
+		},
+		app.OnConnectHandler, app.OnDisconnectHandler,
+	)
+	app.httpServer = HTTPServer.New("httpServer",
+		&Config.HTTPServer{
+			TcpServerConfig: &Config.TcpServer{
+				Port: 8080,
+			},
+		},
+		HTTPServer.Handlers{
+			"/": HTTPServer.SendDirectory("../frontend"),
+		},
+	)
+	Dashboard.NewClient("appWebsocketHttp_dashboardClient",
+		&Config.DashboardClient{
+			ConnectionConfig: &Config.TcpSystemgeConnection{},
+			ClientConfig: &Config.TcpClient{
+				Address: "localhost:60001",
+				TlsCert: Helpers.GetFileContent("MyCertificate.crt"),
+				Domain:  "example.com",
+			},
+		},
+		app.start, app.stop, nil, app.getStatus,
+		nil,
+	).Start()
+	if err := app.start(); err != nil {
+		// shouldn't happen in this sample. Should be properly error handled in a real application though
+		panic(Error.New("Failed to start appWebsocketHttp", err))
+	}
+	return app
 }
 
-func (app *AppWebsocketHTTP) GetCommandHandlers() map[string]Node.CommandHandler {
-	return map[string]Node.CommandHandler{}
+func (app *AppWebsocketHTTP) getStatus() int {
+	return app.status
+}
+
+func (app *AppWebsocketHTTP) start() error {
+	app.statusMutex.Lock()
+	defer app.statusMutex.Unlock()
+	if app.status != Status.STOPPED {
+		return Error.New("App already started", nil)
+	}
+	if err := app.websocketServer.Start(); err != nil {
+		return Error.New("Failed to start websocketServer", err)
+	}
+	if err := app.httpServer.Start(); err != nil {
+		app.websocketServer.Stop()
+		return Error.New("Failed to start httpServer", err)
+	}
+	app.status = Status.STARTED
+	return nil
+}
+
+func (app *AppWebsocketHTTP) stop() error {
+	app.statusMutex.Lock()
+	defer app.statusMutex.Unlock()
+	if app.status != Status.STARTED {
+		return Error.New("App not started", nil)
+	}
+	app.httpServer.Stop()
+	app.websocketServer.Stop()
+	app.status = Status.STOPPED
+	return nil
+}
+
+func (app *AppWebsocketHTTP) WebsocketPropagate(message *Message.Message) {
+	app.websocketServer.Broadcast(message)
+}
+
+func (app *AppWebsocketHTTP) OnConnectHandler(websocketClient *WebsocketServer.WebsocketClient) error {
+	err := websocketClient.Send(Message.NewAsync("connected", websocketClient.GetId()).Serialize())
+	if err != nil {
+		return Error.New("Error sending connected message", err)
+	}
+	return nil
+}
+
+func (app *AppWebsocketHTTP) OnDisconnectHandler(websocketClient *WebsocketServer.WebsocketClient) {
+	response, err := SingleRequestServer.SyncRequest("websocketHttpServer", &Config.SingleRequestClient{
+		TcpConnectionConfig: &Config.TcpSystemgeConnection{},
+		TcpClientConfig: &Config.TcpClient{
+			Address: "localhost:60001",
+			TlsCert: Helpers.GetFileContent("MyCertificate.crt"),
+			Domain:  "example.com",
+		},
+	}, topics.ENDGAME, websocketClient.GetId())
+	if err != nil {
+		panic(Error.New("Error sending endGame request", err))
+	}
+	if response.GetTopic() == Message.TOPIC_SUCCESS {
+		gameId := response.GetPayload()
+		ids := strings.Split(gameId, "-")
+		app.websocketServer.Groupcast(gameId, Message.NewAsync("propagate_gameEnd", ""))
+		app.websocketServer.RemoveClientFromGroup(gameId, ids...)
+	}
 }
